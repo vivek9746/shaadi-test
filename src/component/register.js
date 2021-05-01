0645a7cf067a95bb6cea1d04c12d0bc35a646fa9
vivek9746 <vivekkumarsharma9746@gmail.com>
2021-05-01 12:25:23 +0530

    register validation

--- src/component/register.js
+++ src/component/register.js
@@ -1,48 +1,64 @@
-import React,{useState,useEffect} from 'react'
+import React, { useState, useEffect } from "react";
 
-import { useHistory,Link } from 'react-router-dom'
-import './login.css'
-
+import { useHistory, Link } from "react-router-dom";
+import "./login.css";
 
 function Register() {
-    const history = useHistory()
-    const [name,setName]=useState('')
-    const [password,setPassword] = useState('')
+  const history = useHistory();
+  const [name, setName] = useState("");
+  const [password, setPassword] = useState("");
 
-    function createUser(e){
-        const user = {name:name,
-        password:password}
-            console.log(user)
-        if(!localStorage.getItem(user.name)){
-        localStorage.setItem(user.name,user.password)
-        alert('User Created')
-        history.push('/')
-        }
-      e.preventDefault()
+  function createUser(e) {
+    if (name.length > 0 && password.length > 0) {
+      const user = { name: name, password: password };
+    //   alert(user);
+      if (!localStorage.getItem(user.name)) {
+        localStorage.setItem(user.name, user.password);
+        alert("User Created",user.name);
+        history.push("/");
+      }
+    } else {
+      alert("blank values");
     }
-    return (
-        <div className="loginParent">
-
-        <div className="formParent">
-            <form>
-                <h1>Register</h1>
-                <h4>Name</h4>
-            <input type="text" value={name} onChange={(e)=>{
-                setName(e.target.value)
-            }}></input>
-             <h4>Password</h4>
-            <input type="password" value={password} onChange={(e)=>{
-                setPassword(e.target.value)
-            }}/>
-             <div className="parentOfLogin">
-            <button className="registerButton" type="submit" onClick={(e)=>createUser(e)}>Register</button>
-            <Link to="/"><button className="altButton">Login</button></Link>
-            </div>
-            </form>
-            
-        </div>
-        </div>
-    )
+    e.preventDefault();
+  }
+  return (
+    <div className="loginParent">
+      <div className="formParent">
+        <form>
+          <h1>Register</h1>
+          <h4>Name</h4>
+          <input
+            type="text"
+            value={name}
+            onChange={(e) => {
+              setName(e.target.value);
+            }}
+          ></input>
+          <h4>Password</h4>
+          <input
+            type="password"
+            value={password}
+            onChange={(e) => {
+              setPassword(e.target.value);
+            }}
+          />
+          <div className="parentOfLogin">
+            <button
+              className="registerButton"
+              type="submit"
+              onClick={(e) => createUser(e)}
+            >
+              Register
+            </button>
+            <Link to="/">
+              <button className="altButton">Login</button>
+            </Link>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
 }
 
-export default Register
+export default Register;
